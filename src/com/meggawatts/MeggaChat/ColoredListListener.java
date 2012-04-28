4d7152c3f25d5453f41d59a46dc1dcc53021aa6c
Henry Slawniak <meggawatts@gmail.com>
2012-04-28 01:39:50 -0500

    TAB menu based on permission nodes was silly :\

--- src/com/meggawatts/MeggaChat/ColoredListListener.java
+++ src/com/meggawatts/MeggaChat/ColoredListListener.java
@@ -6,77 +6,81 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import ru.tehkode.permissions.bukkit.PermissionsEx;
+import ru.tehkode.permissions.PermissionUser;
 
 public class ColoredListListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH)
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        if (player.hasPermission("meggachat.color.0")) {
+        PermissionUser target = PermissionsEx.getUser(player);
+        if (target.getOption("ListColor").equals("0")) {
             player.setPlayerListName(ChatColor.BLACK + player.getName());
         }
-        if (player.hasPermission("meggachat.color.1")) {
+        if (target.getOption("ListColor").equals("1")) {
             player.setPlayerListName(ChatColor.DARK_BLUE + player.getName());
         }
-        if (player.hasPermission("meggachat.color.2")) {
+        if (target.getOption("ListColor").equals("2")) {
             player.setPlayerListName(ChatColor.DARK_GREEN + player.getName());
         }
-        if (player.hasPermission("meggachat.color.3")) {
+        if (target.getOption("ListColor").equals("3")) {
             player.setPlayerListName(ChatColor.DARK_AQUA + player.getName());
         }
-        if (player.hasPermission("meggachat.color.4")) {
+        if (target.getOption("ListColor").equals("4")) {
             player.setPlayerListName(ChatColor.DARK_RED + player.getName());
         }
-        if (player.hasPermission("meggachat.color.5")) {
+        if (target.getOption("ListColor").equals("5")) {
             player.setPlayerListName(ChatColor.DARK_PURPLE + player.getName());
         }
-        if (player.hasPermission("meggachat.color.6")) {
+        if (target.getOption("ListColor").equals("6")) {
             player.setPlayerListName(ChatColor.GOLD + player.getName());
         }
-        if (player.hasPermission("meggachat.color.7")) {
+        if (target.getOption("ListColor").equals("7")) {
             player.setPlayerListName(ChatColor.GRAY + player.getName());
         }
-        if (player.hasPermission("meggachat.color.8")) {
+        if (target.getOption("ListColor").equals("8")) {
             player.setPlayerListName(ChatColor.DARK_GRAY + player.getName());
         }
-        if (player.hasPermission("meggachat.color.9")) {
+        if (target.getOption("ListColor").equals("9")) {
             player.setPlayerListName(ChatColor.BLUE + player.getName());
         }
-        if (player.hasPermission("meggachata.color.a")) {
+        if (target.getOption("ListColor").equals("a")) {
             player.setPlayerListName(ChatColor.GREEN + player.getName());
         }
-        if (player.hasPermission("meggachat.color.b")) {
+        if (target.getOption("ListColor").equals("b")) {
             player.setPlayerListName(ChatColor.AQUA + player.getName());
         }
-        if (player.hasPermission("meggachat.color.c")) {
+        if (target.getOption("ListColor").equals("c")) {
             player.setPlayerListName(ChatColor.RED + player.getName());
         }
-        if (player.hasPermission("meggachat.color.d")) {
+        if (target.getOption("ListColor").equals("d")) {
             player.setPlayerListName(ChatColor.LIGHT_PURPLE + player.getName());
         }
-        if (player.hasPermission("meggachat.color.e")) {
+        if (target.getOption("ListColor").equals("e")) {
             player.setPlayerListName(ChatColor.YELLOW + player.getName());
         }
-        if (player.hasPermission("meggachat.color.f")) {
+        if (target.getOption("ListColor").equals("f")) {
             player.setPlayerListName(ChatColor.WHITE + player.getName());
         }
-        if (player.hasPermission("meggachat.color.k")) {
+        if (target.getOption("ListColor").equals("k")) {
             player.setPlayerListName(ChatColor.MAGIC + player.getName());
         }
-        if (player.hasPermission("meggachat.color.l")) {
+        if (target.getOption("ListColor").equals("l")) {
             player.setPlayerListName(ChatColor.BOLD + player.getName());
         }
-        if (player.hasPermission("meggachat.color.m")) {
+        if (target.getOption("ListColor").equals("m")) {
             player.setPlayerListName(ChatColor.STRIKETHROUGH + player.getName());
         }
-        if (player.hasPermission("meggachat.color.n")) {
+        if (target.getOption("ListColor").equals("n")) {
             player.setPlayerListName(ChatColor.UNDERLINE + player.getName());
         }
-        if (player.hasPermission("meggachat.color.o")) {
+        if (target.getOption("ListColor").equals("o")) {
             player.setPlayerListName(ChatColor.ITALIC + player.getName());
         }
-        if (player.hasPermission("meggachat.color.r")) {
+        if (target.getOption("ListColor").equals("r")) {
             player.setPlayerListName(ChatColor.RESET + player.getName());
         }
+
     }
 }
